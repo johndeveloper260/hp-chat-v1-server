9777f3fa1a1bfd3d2af09c00ad6af9b4ea1bb2e1
johndeveloper260 <johndeveloper260@gmail.com>
2026-01-21 23:13:36 +0900

    21Janv2

--- controller/inquiryController.js
+++ controller/inquiryController.js
@@ -131,9 +131,7 @@ export const createInquiry = async (req, res) => {
 // 3. UPDATE
 export const updateInquiry = async (req, res) => {
   const { ticketId } = req.params;
-  const { userId } = req.user.id;
-
-  console.log(req.user);
+  const userId = req.user.id;
 
   const {
     status,
