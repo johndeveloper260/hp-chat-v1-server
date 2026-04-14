e788201243810a90a268d11518353712fe9b631b
johndeveloper260 <johndeveloper260@gmail.com>
2026-04-14 10:30:42 +0900

    Claude2026Apr14v1

--- utils/csv.js
+++ utils/csv.js
@@ -24,9 +24,9 @@ export const formatDate = (val) => {
  */
 export const parseDate = (val) => {
   if (!val || !val.trim()) return null;
-  const m = val.trim().match(/^(\d{4})\/(\d{2})\/(\d{2})$/);
+  const m = val.trim().match(/^(\d{4})\/(\d{1,2})\/(\d{1,2})$/);
   if (!m) return null;
-  return `${m[1]}-${m[2]}-${m[3]}`;
+  return `${m[1]}-${m[2].padStart(2, "0")}-${m[3].padStart(2, "0")}`;
 };
 
 /**
